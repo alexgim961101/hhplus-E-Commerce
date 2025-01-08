5f479d869ad9c85e6ddc9a1258919b0be4eb4048
alex.kim <alex.kim@udptechnology.com>
2025-01-08 12:46:50 +0900

    Refactor: 포인트 충전 메서드 변경 (Post -> Patch)

--- src/point/presentation/point.controller.ts
+++ src/point/presentation/point.controller.ts
@@ -1,14 +1,17 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Patch, Post } from "@nestjs/common";
 import { PointService } from "../domain/point.service";
 import { ChargePointReqDto } from "./dto/charge-point.req.dto";
 import { PointFacade } from "../application/point.facade";
 
-@Controller('point')
+@Controller("point")
 export class PointController {
-    constructor(private readonly pointFacade: PointFacade, private readonly pointService: PointService){}
+  constructor(
+    private readonly pointFacade: PointFacade,
+    private readonly pointService: PointService
+  ) {}
 
-    @Post('charge')
-    async chargePoint(@Body() body: ChargePointReqDto){
-        return await this.pointFacade.chargePoint(body);
-    }
+  @Patch("charge")
+  async chargePoint(@Body() body: ChargePointReqDto) {
+    return await this.pointFacade.chargePoint(body);
+  }
 }
