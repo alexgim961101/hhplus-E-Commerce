1d441ad5fb446a4eef2c07b8875b59721a08b74e
alex <alexgim961101@gmail.com>
2025-01-23 01:42:57 +0900

    Test: 쿠폰 통합 테스트

--- test/it/coupon/coupon-issue.it.spec.ts
+++ test/it/coupon/coupon-issue.it.spec.ts
@@ -4,51 +4,143 @@ import { PrismaService } from '@/prisma/prisma.service';
 import { CouponModule } from '@/coupon/coupon.module';
 import { UserModule } from '@/user/user.module';
 import { PrismaModule } from '@/prisma/prisma.module';
-import { Logger } from '@nestjs/common';
-import { createTable } from '../../common/create-table';
-import { insertData } from '../../common/insert-data';
-import { deleteTable } from '../../common/delete-table';
+import { UserService } from '@/user/domain/service/user.service';
+import { CreateUserReqDto } from '@/user/presentation/dto/request/create-user-req.dto';
+import { DiscountType } from '@prisma/client';
+import { WinstonModule } from 'nest-winston';
+import { winstonConfig } from '@/common/logger/winston.config';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 
 describe('쿠폰 발급 통합 테스트', () => {
+    let userService: UserService;
     let couponFacadeService: CouponFacadeService;
     let prisma: PrismaService;
     let module: TestingModule;
 
-    beforeAll(async () => {
+    beforeEach(async () => {
         module = await Test.createTestingModule({
-            imports: [CouponModule, UserModule, PrismaModule]
+            imports: [UserModule, CouponModule, PrismaModule, WinstonModule.forRoot(winstonConfig)]
         }).compile();
-        module.useLogger(new Logger());
+
+        userService = module.get<UserService>(UserService);
         couponFacadeService = module.get<CouponFacadeService>(CouponFacadeService);
         prisma = module.get<PrismaService>(PrismaService);
-    });
 
-    beforeEach(async () => {
-        // 테이블 생성 SQL 실행
-        await createTable(prisma);
-
-        // 초기 데이터 삽입 SQL 실행
-        await insertData(prisma);
-    });
-
-    afterEach(async () => {
-        // 테이블 삭제
-        await deleteTable(prisma);
-    });
-
-    afterAll(async () => {
-        await module.close();
+        await prisma.user.deleteMany();
+        await prisma.coupon.deleteMany();
     });
 
     describe('issueCoupon', () => {
-        it('쿠폰 발급이 성공적으로 이루어져야 한다', async () => {});
+        it('쿠폰 발급이 성공적으로 이루어져야 한다', async () => {
+            // given
+            // 1. 사용자 생성
+            const user = await userService.createUser(new CreateUserReqDto());
 
-        it('존재하지 않는 사용자의 경우 예외가 발생해야 한다', async () => {});
+            // 2. 쿠폰 생성
+            const coupon = await prisma.coupon.create({
+                data: {
+                    id: 1,
+                    title: 'test coupon',
+                    description: 'test',
+                    discountType: DiscountType.PERCENTAGE,
+                    discountAmount: 10,
+                    validFrom: new Date(Date.now() - 1000 * 60 * 60 * 24 * 30),
+                    validTo: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30 * 2),
+                    maxCount: 10,
+                    currentCount: 0
+                }
+            })
 
-        it('품절된 쿠폰의 경우 발급되지 않아야 한다', async () => {});
+            // when
+            const issuedCoupon = await couponFacadeService.issueCoupon(user.id, coupon.id);
+
+            // then
+            expect(issuedCoupon.title).toBe(coupon.title);
+            expect(issuedCoupon.currentCount).toBe(1);
+        });
+
+        it('존재하지 않는 사용자의 경우 예외가 발생해야 한다', async () => {
+            // given
+            // 1. 사용자 생성
+            const user = await userService.createUser(new CreateUserReqDto());
+
+            // 2. 쿠폰 생성
+            const coupon = await prisma.coupon.create({
+                data: {
+                    id: 1,
+                    title: 'test coupon',
+                    description: 'test',
+                    discountType: DiscountType.PERCENTAGE,
+                    discountAmount: 10,
+                    validFrom: new Date(Date.now() - 1000 * 60 * 60 * 24 * 30),
+                    validTo: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30 * 2),
+                    maxCount: 10,
+                    currentCount: 0
+                }
+            })
+
+            // when && then
+            await expect(couponFacadeService.issueCoupon(2, coupon.id)).rejects.toThrow(NotFoundException);
+        });
+
+        it('품절된 쿠폰의 경우 발급되지 않아야 한다', async () => {
+            // given
+            // 1. 사용자 생성
+            const user = await userService.createUser(new CreateUserReqDto());
+
+            // 2. 쿠폰 생성
+            const coupon = await prisma.coupon.create({
+                data: {
+                    id: 1,
+                    title: 'test coupon',
+                    description: 'test',
+                    discountType: DiscountType.PERCENTAGE,
+                    discountAmount: 10,
+                    validFrom: new Date(Date.now() - 1000 * 60 * 60 * 24 * 30),
+                    validTo: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30 * 2),
+                    maxCount: 10,
+                    currentCount: 10
+                }
+            })
+
+            // when && then
+            await expect(couponFacadeService.issueCoupon(user.id, coupon.id)).rejects.toThrow(BadRequestException);
+        });
 
         describe('동시성 테스트', () => {
-            it('동시에 여러 요청이 들어와도 정확한 수량만큼만 발급되어야 한다', async () => {});
+            it('동시에 여러 요청이 들어와도 정확한 수량만큼만 발급되어야 한다', async () => {
+                // given
+                const user = await userService.createUser(new CreateUserReqDto());
+
+                const coupon = await prisma.coupon.create({
+                    data: {
+                        id: 1,
+                        title: 'test coupon',
+                        description: 'test',
+                        discountType: DiscountType.PERCENTAGE,
+                        discountAmount: 10,
+                        validFrom: new Date(Date.now() - 1000 * 60 * 60 * 24 * 30),
+                        validTo: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30 * 2),
+                        maxCount: 10,
+                        currentCount: 5
+                    }
+                })
+
+                // when
+                await Promise.all([
+                    couponFacadeService.issueCoupon(user.id, coupon.id),
+                    couponFacadeService.issueCoupon(user.id, coupon.id),
+                    couponFacadeService.issueCoupon(user.id, coupon.id)
+                ]);
+
+                
+
+                // then
+                const issuedCoupon = await prisma.coupon.findUnique({
+                    where: { id: coupon.id }
+                });
+                expect(issuedCoupon.currentCount).toBe(8);
+            });
         });
     });
 }); 
