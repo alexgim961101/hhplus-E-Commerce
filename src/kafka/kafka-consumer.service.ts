4dc61194869b44acc8b1e86599be7c98d785ae34
alex <alexgim961101@gmail.com>
2025-02-20 00:50:55 +0900

    Fix: Change logger (console -> winston)

--- src/kafka/kafka-consumer.service.ts
+++ src/kafka/kafka-consumer.service.ts
@@ -1,11 +1,15 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Inject } from "@nestjs/common";
 import { MessagePattern } from "@nestjs/microservices";
+import { WINSTON_MODULE_PROVIDER } from "nest-winston";
+import { Logger } from "winston";
 
 @Controller()
 export class KafkaConsumerController {
 
+    constructor(@Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger) {}
+
     @MessagePattern('test')
     handleTest(data: any) {
-        console.log(`Received message: ${JSON.stringify(data)}`);
+        this.logger.info(`Received message: ${JSON.stringify(data)}`);
     }
 }
